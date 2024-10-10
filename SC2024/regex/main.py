7c8bb245650bb4d451a77981c454703128fdd03a
M00nWol <ansusthf@gmail.com>
2024-10-10 16:24:38 +0900

    sc) 대소문자 관련 정규표현식 구현

--- SC2024/regex/main.py
+++ SC2024/regex/main.py
@@ -1,24 +1,13 @@
+## 추출하고 싶다!
+
 import re
 
-buf = """<SCRIPT>
-function doSpellCheck(form, field) {
-  // Make sure not empty
-  if (field.value == '') {
-    return false;
-  }
-  //Init
-  var windowName='spellWindow';
-  var
-  spellCheckURL='spell.cfm?formname=Comment&fieldname='+field.name;
-  //Done
-  return false
-}
-</SCRIPT>
+buf = """Python python
+Pythonpython
+python, Python
 """
 
-# re.match를 re.search로 변경
-ptn = re.compile(r"<SCRIPT>([\d\D]*?)<\/SCRIPT>", re.DOTALL)
+ptn = re.compile(r"([Pp]ython(, )?)+")
 
-result = ptn.search(buf)  # search를 사용하여 전체를 탐색
-if result:
-    print(result.group())  # 매칭된 전체 결과를 출력
+for result in ptn.finditer(buf):
+    print(result)
