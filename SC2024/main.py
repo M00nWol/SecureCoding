570e89f89f6aad4b914e905ba32ff8110028cae9
M00nWol <ansusthf@gmail.com>
2024-10-10 15:46:46 +0900

    sc) password에서 조건 가져오기

--- SC2024/main.py
+++ SC2024/main.py
@@ -1,12 +1,12 @@
 import re
 
-buf = """Send personal email to ben@nurilab.com. For questions about a book use support@nurilab.com. Feel free to send unsolicited email to spam@nurilab.com (wouldn't it be nice if it were that sample, huh?).kim.su@gsad.co.kr"""
+buf = "password123"
 
-ptn = re.compile(r"(\w[\w.-]*)@\w+(\.\w+)+")
+ptn = re.compile(r"\w{5,}")
 
-result = ptn.search(buf)
+# search를 사용하면 ! 이후만 가져옴
+# result = ptn.search(buf)
 
-for p in ptn.finditer(buf):
-    print(p.group())
-    # print(p.groups()[0])   # 최초 탐지 결과
-    print(p.span())    # 최초 탐지 위치    
+# match 쓰면 pw 조건 아니면 안 가져옴 : 처음부터 일치하는지 확인하는 목적
+result = ptn.match(buf)
+print(result)
